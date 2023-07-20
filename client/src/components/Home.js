45c9667f0dc882079ab2317b2191bc152fec5972
Tuvia Nacshonov <tuvian7@gmail.com>
2023-07-19 22:07:20 -0400

    Added theme and starting template

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
+import Skills from './Skills'
 
 function Home() {
+    // state variables
+    const [showSkills, setShowSkills] = useState(false);
+
     return (
-        <div class="home">
-            <h1>Welcome!</h1>
-        </div>
+        <>            
+            <div className="header">
+                    <h1>Tuvia Nacshonov</h1>
+                    <h3>Full Stack Developer</h3>
+                </div>
+                <div className="options">
+                    <ul>
+                        <li onClick={() => setShowSkills(true)}>
+                            SKILLS
+                        </li>
+                        <li>ABOUT ME</li>
+                    </ul>
+                </div>
+                <div className="skills">
+                    {!showSkills && (
+                        <div>
+                        </div>
+
+                    )}
+                    {showSkills && <Skills />}
+                </div>
+        </>
     );
 }
 
---
+++ client/src/components/Skills.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './Home.css'
+import Skills from './Skills'
+
+function Home() {
+    // state variables
+
+    return (
+        <>
+        <h3>hello</h3>
+        </>
+    );
+}
+
+export default Home;
